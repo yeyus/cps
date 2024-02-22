1b22ef45e28fff5673ae30379e25833cc47d7091
Jesus Trujillo <elyeyus@gmail.com>
2024-02-21 17:37:29 -0800

    some wip codeplug file generation and sample decoding

--- src/modules/radio-downloader/radio-downloader.tsx
+++ src/modules/radio-downloader/radio-downloader.tsx
@@ -2,6 +2,22 @@ import * as React from "react";
 import { TransferEmitter } from "../../utils/transfer-emitter";
 import { useConnection } from "../connection-manager/context";
 import { ConnectionStatus } from "../connection-manager/types";
+import { QuanshengUVK5MemoryMap } from "../../configs/radios/quansheng/uvk5/memory-map";
+
+function SaveToDiskButton({ codeplug }: { codeplug: Uint8Array }) {
+  const downloadUrl = React.useMemo(() => {
+    const blob = new Blob([codeplug], { type: "application/octet-stream" });
+    return URL.createObjectURL(blob);
+  }, [codeplug]);
+
+  return (
+    <button type="button" disabled={codeplug == null}>
+      <a download="codeplug.bin" target="_blank" rel="noreferrer" href={downloadUrl}>
+        Save Codeplug
+      </a>
+    </button>
+  );
+}
 
 export default function RadioDownloader() {
   const [total, setTotal] = React.useState(0);
@@ -9,6 +25,8 @@ export default function RadioDownloader() {
   const emitterRef = React.useRef<TransferEmitter | undefined>(undefined);
   const { state: connection } = useConnection();
   const isConnected = connection.status === ConnectionStatus.CONNECTED && connection.radio !== null;
+
+  const [codeplug, setCodeplug] = React.useState<Uint8Array>();
 
   React.useEffect(() => {
     // debug
@@ -26,8 +44,16 @@ export default function RadioDownloader() {
     setCurrent(emitter.current);
   });
 
-  const handleDownload = () => {
-    connection.radio?.downloadCodeplug(emitterRef.current);
+  const handleDownload = async () => {
+    const bytes = await connection.radio?.downloadCodeplug(emitterRef.current);
+    setCodeplug(bytes);
+  };
+
+  const handleDecode = () => {
+    if (codeplug === undefined) return;
+
+    const memoryMap = QuanshengUVK5MemoryMap.fromBuffer(codeplug);
+    console.log(`The memory map`, memoryMap);
   };
 
   return (
@@ -35,9 +61,13 @@ export default function RadioDownloader() {
       <button type="button" onClick={handleDownload} disabled={!isConnected}>
         Download Codeplug
       </button>
+      <button type="button" onClick={handleDecode} disabled={codeplug === undefined}>
+        Decode Codeplug
+      </button>
       <span>
         {current}/{total}
       </span>
+      {codeplug != null ? <SaveToDiskButton codeplug={codeplug} /> : null}
     </>
   );
 }
